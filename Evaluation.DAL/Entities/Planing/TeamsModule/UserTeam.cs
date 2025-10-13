4e4817b710446935d3d9eacec81eef0a2486ea93
Feras Mutasem Dev <t-f.mutasem-Dev@edu.gov.qa>
2025-10-13 14:59:10 +0300

    added UserTeam

---
+++ Evaluation.DAL/Entities/Planing/TeamsModule/UserTeam.cs
@@ -0,0 +1,9 @@
+﻿using Evaluation.DAL.Entities.BaseModule;
+
+namespace Evaluation.DAL.Entities.Planing.TeamsModule;
+
+public class UserTeam : BaseEntities
+{
+    public Guid TeamId { get; set; }
+    public Guid UserId { get; set; }
+}
