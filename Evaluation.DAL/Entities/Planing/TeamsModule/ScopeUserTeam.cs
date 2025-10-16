41bc769fb271de3d87bc54fec84f970e0d3a426a
Feras Mutasem Dev <t-f.mutasem-Dev@edu.gov.qa>
2025-10-16 10:19:26 +0300

    Scope User Team handle the format

--- Evaluation.DAL/Entities/Planing/TeamsModule/ScopeUserTeam.cs
+++ Evaluation.DAL/Entities/Planing/TeamsModule/ScopeUserTeam.cs
@@ -1,13 +1,15 @@
 ﻿using Evaluation.DAL.Entities.BaseModule;
 using Evaluation.DAL.Entities.Authentication;
+using Evaluation.DAL.Entities.FormsModules;
 
 namespace Evaluation.DAL.Entities.Planing.TeamsModule;
 
 public class ScopeUserTeam : EntityBase
 {
     public Guid UserId { get; set; }
+    public User User { get; set; } = null!;
+    public Guid ScopeId { get; set; }
+    public Scope Scope { get; set; } = null!;
     public Guid TeamId { get; set; }
-    public Guid EvaluationFieldId { get; set; }
-    public User? User { get; set; }
-    public Team? Team { get; set; }
+    public Team Team { get; set; } = null!;
 }
