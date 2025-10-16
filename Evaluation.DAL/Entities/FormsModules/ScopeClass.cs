5b55a98d9f7ba499540d8fddefa9503756e13ef0
Feras Mutasem Dev <t-f.mutasem-Dev@edu.gov.qa>
2025-10-16 09:57:25 +0300

    Added OrgTreeClass

--- Evaluation.DAL/Entities/FormsModules/ScopeClass.cs
+++ Evaluation.DAL/Entities/FormsModules/ScopeClass.cs
@@ -2,12 +2,12 @@
 
 namespace Evaluation.DAL.Entities.FormsModules;
 
-public class ScopeType
+public class ScopeClass
 {
     public string NameAr { get; set; } = null!;
     public string NameEn { get; set; } = null!;
     public int ParentId { get; set; }
-    public ScopeType? Parent { get; set; }
+    public ScopeClass? Parent { get; set; }
     public int DepartmentId { get; set; }
     public Department? Department { get; set; }
     public int OrderNo { get; set; }
--- Evaluation.DAL/Entities/Org/OrgTree.cs
+++ Evaluation.DAL/Entities/Org/OrgTree.cs
@@ -1,5 +1,4 @@
 ﻿using Evaluation.DAL.Entities.BaseModule;
-using Evaluation.DAL.Entities.Planing;
 
 namespace Evaluation.DAL.Entities.Org;
 
@@ -7,8 +6,10 @@ public class OrgTree : EntityBase
 {
     public string NameAr { get; set; } = null!;
     public string NameEn { get; set; } = null!;
-    public Guid ParentTreeId { get; set; }
-    public OrgTree? ParentTree { get; set; }
+    public Guid OrgTreeId { get; set; }
+    public OrgTree? Parent { get; set; }
     public string? HrCode { get; set; } 
-    public string? NSISCode { get; set; } 
+    public string? NSISCode { get; set; }
+    public Guid OrgTreeTypeId { get; set; }
+    public OrgTreeClass OrgTreeClass { get; set; } = null!;
 }
