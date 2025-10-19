03b58393300df269b546ef6f45582fac1f7214fa
Aya Abdalla <t-a.abdalla@edu.gov.qa>
2025-10-19 10:28:13 +0300

    class ExceptionLog

---
+++ Evaluation.DAL/Entities/Exception/ExceptionLog.cs
@@ -0,0 +1,19 @@
+﻿using Evaluation.DAL.Entities.BaseModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluation.DAL.Entities.Exception
+{
+    public class ExceptionLog : EntityBase
+    {
+        public string ExceptionType { get; set; } = null!;
+        public string Message { get; set; } = null!;
+        public string StackTrace { get; set; } = null!;
+        public DateTime Timestamp { get; set; }
+        public string? JsonParameter { get; set; }
+        public string? Context { get; set; }
+    }
+}
