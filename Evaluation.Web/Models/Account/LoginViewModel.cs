d44ddadbb4dc1c932f9662cf593be2e203a4794d
Aya Abdalla <t-a.abdalla@edu.gov.qa>
2025-10-21 11:07:15 +0300

    Login view

---
+++ Evaluation.Web/Models/Account/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Evaluation.Web.Models.Account
+{
+    public class LoginViewModel
+    {
+        public string? RedirectUrl { get; set; }
+        public string Email { get; set; } = null!;
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+
+    }
+}
