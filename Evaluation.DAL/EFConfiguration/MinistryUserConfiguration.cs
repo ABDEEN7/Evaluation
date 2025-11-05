dd5f8033c0e9ab057f266498c3f1638f436d85f4
t-m.abutarbush <t-m.abutarbush@edu.gov.qa>
2025-11-05 15:13:25 +0300

    remove user type

--- Evaluation.DAL/EFConfiguration/MinistryUserConfiguration.cs
+++ Evaluation.DAL/EFConfiguration/MinistryUserConfiguration.cs
@@ -8,7 +8,6 @@ namespace Evaluation.DAL.EFConfiguration
     {
         public void Configure(EntityTypeBuilder<MinistryUser> builder)
         {
-            builder.HasOne(x => x.UserType).WithMany().HasForeignKey(x => x.UserTypeId);
             builder.HasOne(c => c.CreateBy).WithMany().HasForeignKey(y => y.CreateById).IsRequired(false);
 
             builder.HasOne(c => c.UpdateBy).WithMany().HasForeignKey(c => c.UpdateById);
