8447ba80efdd8397776a533aebb90e2f018a1e22
Thanveera Koornadka <t-k.thanveera-dev@edu.gov.qa>
2025-11-11 14:38:32 +0300

    AdminSetup

---
+++ Evaluation.Services/Mappers/Admin/ControlValidationProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Evaluation.DAL.Entities.BaseModule;
+using Evaluation.SharedHelper.Models.Admin;
+
+
+namespace Evaluation.Services.Mappers
+{
+    public class ControlValidationProfile : Profile
+    {
+        public ControlValidationProfile()
+        {
+            CreateMap<ControlValidation, ControlValidationDTO>();
+        }
+    }
+}
