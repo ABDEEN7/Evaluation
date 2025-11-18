812dc897925ec7e8fa5b45db58d21587a396f21b
Mohamed Fatouh (EBLA) <t-m.fatouh-dev@edu.gov.qa>
2025-11-18 10:27:13 +0300

    DB Migartion 20251118072439_MakeNullOrgClassParent

--- Evaluation.DAL/Models/Org/OrgClass.cs
+++ Evaluation.DAL/Models/Org/OrgClass.cs
@@ -4,11 +4,11 @@ using Microsoft.EntityFrameworkCore;
 namespace Evaluation.DAL.Models.Org;
 [Index(nameof(BackendName), IsUnique = true)]
 public class OrgClass : EntityBase
-{
-    public Guid ParentId { get; set; }
-    public OrgClass? Parent { get; set; }
+{  
     public string NameAr { get; set; } = null!;
     public string NameEn { get; set; } = null!;
     public string BackendName { get; set; } = null!;
     public string? HRCode { get; set; }
+    public Guid? ParentId { get; set; }
+    public OrgClass? Parent { get; set; }
 }
