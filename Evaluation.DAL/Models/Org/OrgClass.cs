0ee7685476ff9aa4bd4dd4dc689b4adfdda2724d
t-m.abutarbush <t-m.abutarbush@edu.gov.qa>
2025-11-18 10:55:16 +0300

    OrgClass ParentId Nullable

--- Evaluation.DAL/Models/Org/OrgClass.cs
+++ Evaluation.DAL/Models/Org/OrgClass.cs
@@ -5,7 +5,7 @@ namespace Evaluation.DAL.Models.Org;
 [Index(nameof(BackendName), IsUnique = true)]
 public class OrgClass : EntityBase
 {
-    public Guid ParentId { get; set; }
+    public Guid? ParentId { get; set; }
     public OrgClass? Parent { get; set; }
     public string NameAr { get; set; } = null!;
     public string NameEn { get; set; } = null!;
