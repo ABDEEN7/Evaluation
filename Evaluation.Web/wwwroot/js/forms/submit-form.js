4339dd91a657a21973427b3c9b5e2b79a3045a2c
Feras Mutasem Dev <t-f.mutasem-Dev@edu.gov.qa>
2025-12-09 13:20:01 +0300

    Form Items

---
+++ Evaluation.Web/wwwroot/js/forms/submit-form.js
@@ -0,0 +1,82 @@
+﻿
+$(document).ready(function () {
+    $("#btnSubmitForm").on("click", function (e) {
+        e.preventDefault();
+        submitEvaluationForm();
+    });
+});
+function submitEvaluationForm() {
+    const formId = "e4530010-a302-41f5-935c-2599674db37a";
+    const mainItems = [];
+
+    // ========== LOOP MAIN ITEMS ONLY ==========
+    $("#tbodyRows tr.main-row").each(function () {
+
+        const row = $(this);
+        const select = row.find("select.eval-select");
+
+        const mainId = select.data("id");
+
+        const selectedValue =
+            select.find("option:selected").data("id") ||
+            select.val() ||
+            null;
+
+        const note =
+            row.find("textarea.note-input").val() || null;
+
+        const mainObj = {
+            id: mainId,
+            value: selectedValue,
+            note: note,
+            subItems: []
+        };
+
+        // ========== LOOP SUB ITEMS RELATED TO THIS MAIN ==========
+        $(`tr.child-row[data-parent-id="${mainId}"]`).each(function () {
+
+            const childRow = $(this);
+            const childSelect = childRow.find("select.eval-select");
+
+            const childId = childSelect.data("id");
+
+            const childValue =
+                childSelect.find("option:selected").data("id") ||
+                childSelect.val() ||
+                null;
+
+            const childnote =
+                childRow.find("textarea.note-input").val() || null;
+
+            mainObj.subItems.push({
+                id: childId,
+                value: childValue,
+                note: childnote
+            });
+        });
+
+        mainItems.push(mainObj);
+    });
+
+    // Strengths & Improvements
+    const strengths = $("textarea[data-row='1']").eq(0).val() || null;
+    const improvements = $("textarea[data-row='1']").eq(1).val() || null;
+
+    const payload = {
+        id: formId,
+        items: mainItems,
+        strengths: strengths,
+        improvements: improvements
+    };
+
+    console.log("FINAL NESTED JSON:", payload);
+
+    jqClient().Post("/Form/SaveEvaluation", payload)
+        .done((res) => {
+            Swal.fire({
+                icon: "success",
+                title: "تم الإرسال",
+                text: "تم إرسال الاستمارة للاعتماد بنجاح"
+            });
+        });
+}
