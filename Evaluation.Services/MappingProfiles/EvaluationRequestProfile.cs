f0e222e72ae535cc3e22913b27f547bcfd06296d
t-m.abutarbush <t-m.abutarbush@edu.gov.qa>
2025-12-14 15:21:32 +0300

    add Full Calender

---
+++ Evaluation.Services/MappingProfiles/EvaluationRequestProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Evaluation.DAL.Models.Planing.EvaluationRequestEntity;
+using Evaluation.SharedHelper.Dtos.PlanDto;
+
+
+namespace Evaluation.Services.MappingProfiles;
+
+
+
+    public class EvaluationRequestProfile : Profile
+{
+    public EvaluationRequestProfile()
+    {
+        CreateMap<EvaluationRequest, EvaluationRequestCalenderDto>()
+            .ForMember(d => d.Title, opt => opt.MapFrom(src => src.OrgTree.NameEn + $" ({src.Plan.PlanName}) - {src.DepEvaluationType.NameEn}"))
+            .ForMember(d => d.Start, opt => opt.MapFrom(src => src.FromDate.ToString("yyyy-MM-dd")))
+            .ForMember(d => d.End, opt => opt.MapFrom(src => src.ToDate.ToString("yyyy-MM-dd")))
+            .ReverseMap();
+    }
+}
