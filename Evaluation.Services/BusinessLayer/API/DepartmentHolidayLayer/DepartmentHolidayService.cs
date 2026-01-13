e15f3998aa9a3bffe4ba6fa39edb035b95ae15ce
Feras Mutasem Dev <t-f.mutasem-Dev@edu.gov.qa>
2026-01-13 07:57:47 +0300

    Handle Department Holiday

--- Evaluation.Services/BusinessLayer/API/DepartmentHolidayLayer/DepartmentHolidayService.cs
+++ Evaluation.Services/BusinessLayer/API/DepartmentHolidayLayer/DepartmentHolidayService.cs
@@ -2,10 +2,12 @@
 using Evaluation.DAL.Dtos;
 using Evaluation.DAL.Helper;
 using Evaluation.DAL.Models.Calendars;
+using Evaluation.DAL.Models.Master;
 using Evaluation.DAL.Repositories;
 using Evaluation.Services.Special;
+using Evaluation.SharedHelper.Dtos.EvalFormDto;
 using Evaluation.SharedHelper.Models;
-using FluentResults;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Evaluation.Services.BusinessLayer.API.DepartmentHolidayLayer;
@@ -17,26 +19,21 @@ public class DepartmentHolidayService(IServiceScopeFactory serviceScopeFactory,
     IMapper mapper,
     UserInfo userInfo,
     IServiceProvider serviceProvider,
-    RequestInfo requestInfo,
-    DepartmentHolidayRepository departmentHolidayRepository
-    ) : ApiBase(serviceScopeFactory, cacheDataProvider, unitOfWork, loggingServices, mapper, userInfo,
-        serviceProvider, requestInfo)
+    RequestInfo requestInfo
+    ) : ApiBase(serviceScopeFactory, cacheDataProvider, unitOfWork, loggingServices, mapper, userInfo, serviceProvider, requestInfo)
 {
-    public async Task<Result<List<DepartmentHolidayDto>>> GetDepartmentHoliday(Guid AcademicYearId)
+    public async Task<List<DepartmentHoliday>> GetDepartmentHolidayList(int Page)
     {
-        return await ExecuteWithResult(async () =>
-        {
-            List<DepartmentHoliday> model = await departmentHolidayRepository.GetDepartmentHolidays(AcademicYearId);
-            List<DepartmentHolidayDto> result = model.Select(x => new DepartmentHolidayDto
-            {
-                Id = x.Id,
-                StartDate = x.StartDate,
-                Name = x.NameEn,
-                IsCronExpression = x.IsCronExpression,
-                CronExpression = x.CronExpression,
-                EndDate = x.EndDate.HasValue ? x.EndDate.Value : null,
-            }).ToList();
-            return result;
-        });
+        var list = await unitOfWork.GetRepository<DepartmentHoliday>()
+                .GetAllNonDeleted()
+                .Include(x => x.CreateBy)
+                .OrderByDescending(x => x.CreateDate)
+                 .Skip(Page * 20)
+                .Take(20)
+                .ToListAsync();
+        return list;
+
+
     }
+
 }
