c5fc5cab359d4ccdab077cfaf1efc1845977d9fe
Feras Mutasem Dev <t-f.mutasem-Dev@edu.gov.qa>
2026-01-19 09:56:00 +0300

    handle isActive isDelete and CreateBy to be not null

--- Evaluation.Services/MappingProfiles/EvalFormProfile.cs
+++ Evaluation.Services/MappingProfiles/EvalFormProfile.cs
@@ -15,8 +15,8 @@ public class EvalFormProfile : Profile
     public EvalFormProfile()
     {
         CreateMap<EvalForm, EvaluationFormDto>()
-           .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => src.IsActive ?? true))
-                .ForMember(dest => dest.IsDeleted, opt => opt.MapFrom(src => src.IsDeleted ?? false))
+           .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => src.IsActive))
+                .ForMember(dest => dest.IsDeleted, opt => opt.MapFrom(src => src.IsDeleted))
                 .ForMember(dest => dest.UpdateBy, opt => opt.MapFrom<UserProfileResolver, Guid?>(src => src.UpdateById.HasValue ? src.UpdateById : src.CreateById))
                .ForMember(dest => dest.UpdateDate, opt => opt.MapFrom(src => src.UpdateDate.HasValue ? src.UpdateDate.Value.ToString("yyyy-MM-dd hh:mm:ss tt") : src.CreateDate.ToString("yyyy-MM-dd hh:mm:ss tt")))
                 .ForMember(dest => dest.EvalFormType, opt => opt.MapFrom<EvalFormTypeResolver, Guid?>(src => src.EvalFormTypeId))
