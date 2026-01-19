a2421ca7d847d25c88f2b9bd9b5a7759d5d2de85
Feras Mutasem Dev <t-f.mutasem-Dev@edu.gov.qa>
2026-01-19 13:11:20 +0300

    handle profile issues

--- Evaluation.Services/Mappers/Admin/FormEvalMarixValueProfile.cs
+++ Evaluation.Services/Mappers/Admin/FormEvalMarixValueProfile.cs
@@ -11,7 +11,7 @@ namespace Evaluation.Services.Mappers.Admin
     {
         public FormEvalMarixValueProfile()
         {
-            CreateMap<FormEvalMarixValue, FormEvalMarixValueDTO>()
+            CreateMap<FormEvalMatrixValue, FormEvalMarixValueDTO>()
                 .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => src.IsActive))
                 .ForMember(dest => dest.IsDeleted, opt => opt.MapFrom(src => src.IsDeleted))
                  .ForMember(dest => dest.UpdateBy, opt => opt.MapFrom<UserProfileResolver, Guid?>(src => src.UpdateById.HasValue ? src.UpdateById : src.CreateById))
