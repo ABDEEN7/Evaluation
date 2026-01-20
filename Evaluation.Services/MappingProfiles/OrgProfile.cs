744eb0657b9aa0a7784223ca18a3b3590e955507
Feras Mutasem Dev <t-f.mutasem-Dev@edu.gov.qa>
2026-01-20 14:57:47 +0300

    comment code

--- Evaluation.Services/MappingProfiles/OrgProfile.cs
+++ Evaluation.Services/MappingProfiles/OrgProfile.cs
@@ -1,31 +1,31 @@
-﻿using AutoMapper;
-using Evaluation.DAL.Dtos.Form;
-using Evaluation.DAL.Models.FormsModules;
-using Evaluation.DAL.Models.Org;
-using Evaluation.Services.Mappers.Admin;
-using Evaluation.SharedHelper.Dtos.OrgDto;
+﻿//using AutoMapper;
+//using Evaluation.DAL.Dtos.Form;
+//using Evaluation.DAL.Models.FormsModules;
+//using Evaluation.DAL.Models.Org;
+//using Evaluation.Services.Mappers.Admin;
+//using Evaluation.SharedHelper.Dtos.OrgDto;
 
-namespace Evaluation.Services.MappingProfiles;
+//namespace Evaluation.Services.MappingProfiles;
 
-public class OrgProfile : Profile
-{
-    public OrgProfile()
-    {
-        CreateMap<School, OrgDetailsDto>()
-            .ForMember(d => d.Id, opt => opt.MapFrom(src => src.Id))
-            .ForMember(d => d.Name, opt => opt.MapFrom(src => src.NameEn))
-            .ReverseMap();
+//public class OrgProfile : Profile
+//{
+//    public OrgProfile()
+//    {
+//        CreateMap<School, OrgDetailsDto>()
+//            .ForMember(d => d.Id, opt => opt.MapFrom(src => src.Id))
+//            .ForMember(d => d.Name, opt => opt.MapFrom(src => src.NameEn))
+//            .ReverseMap();
 
-        CreateMap<Employee, OrgDetailsDto>()
-            .ForMember(d => d.Id, opt => opt.MapFrom(src => src.Id))
-            .ForMember(d => d.Name, opt => opt.MapFrom(src => src.NameEn))
-            .ForMember(d => d.JobTitle, opt => opt.MapFrom<JobTitleResolver, Guid?>(src => src.JobTitleId))
-            .ForMember(d => d.UserGender, opt => opt.MapFrom<UserGenderResolver, Guid?>(src => src.UserGenderId))
-            .ReverseMap();
+//        CreateMap<Employee, OrgDetailsDto>()
+//            .ForMember(d => d.Id, opt => opt.MapFrom(src => src.Id))
+//            .ForMember(d => d.Name, opt => opt.MapFrom(src => src.NameEn))
+//            .ForMember(d => d.JobTitle, opt => opt.MapFrom<JobTitleResolver, Guid?>(src => src.JobTitleId))
+//            .ForMember(d => d.UserGender, opt => opt.MapFrom<UserGenderResolver, Guid?>(src => src.UserGenderId))
+//            .ReverseMap();
 
-        CreateMap<Organization, OrgDetailsDto>()
-            .ForMember(d => d.Id, opt => opt.MapFrom(src => src.Id))
-            .ForMember(d => d.Name, opt => opt.MapFrom(src => src.NameEn))
-            .ReverseMap();
-    }
-}
+//        CreateMap<Organization, OrgDetailsDto>()
+//            .ForMember(d => d.Id, opt => opt.MapFrom(src => src.Id))
+//            .ForMember(d => d.Name, opt => opt.MapFrom(src => src.NameEn))
+//            .ReverseMap();
+//    }
+//}
