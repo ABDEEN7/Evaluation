437f5e2ad2c117cd0731950e43b5ee2dda19f4ee
Thanveera Koornadka <t-k.thanveera-dev@edu.gov.qa>
2026-01-28 11:12:50 +0300

    CD for DepTargetOrgTree

--- Evaluation.SharedHelper/Enums/ConstantKeys.cs
+++ Evaluation.SharedHelper/Enums/ConstantKeys.cs
@@ -341,6 +341,7 @@ public static class ConstantKeys
         public static readonly string AdminSystemSetting = "AdminSystemSetting";
         public static readonly string AdminUiControl = "AdminUiControl";
         public static readonly string AdminNDAStatus = "AdminNDAStatus";
+        public static readonly string AdminDepTargetOrgTree = "AdminDepTargetOrgTree";
     }
     public static class WebAppPages
     {
@@ -848,6 +849,15 @@ public static class ConstantKeys
         public const string EDIT_ADMIN_NDAStatus = "EDIT_ADMIN_NDASTATUS";
         public const string DELETE_ADMIN_NDAStatus = "DELETE_ADMIN_NDASTATUS";
         public const string UPDATE_ORDER_ADMIN_NDAStatus = "UPDATE_ORDER_ADMIN_NDASTATUS";
+        #endregion
+
+        #region DEPTARGETORGTREE
+
+        public const string VIEW_ADMIN_DEPTARGETORGTREE = "VIEW_ADMIN_DEPTARGETORGTREE";
+        public const string ADD_ADMIN_DEPTARGETORGTREE = "ADD_ADMIN_DEPTARGETORGTREE";
+        public const string EDIT_ADMIN_DEPTARGETORGTREE = "EDIT_ADMIN_DEPTARGETORGTREE";
+        public const string DELETE_ADMIN_DEPTARGETORGTREE = "DELETE_ADMIN_DEPTARGETORGTREE";
+
         #endregion
 
     }
