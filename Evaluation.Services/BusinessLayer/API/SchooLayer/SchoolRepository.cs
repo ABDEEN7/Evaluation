ca4cc753d331d5962cc7af9d70ff15da2ede8895
Feras Mutasem Dev <t-f.mutasem-Dev@edu.gov.qa>
2026-01-29 10:38:03 +0300

    resolv issues

--- Evaluation.Services/BusinessLayer/API/SchooLayer/SchoolRepository.cs
+++ Evaluation.Services/BusinessLayer/API/SchooLayer/SchoolRepository.cs
@@ -87,7 +87,7 @@ public class SchoolRepository(IServiceScopeFactory serviceScopeFactory,
 
         Expression<Func<School, bool>> filter = s => true;
         filter = filter.And(c => targetOrgTreeIds.Contains(c.OrgParentId) && currentSchools.Contains(c.Id));
-        if (request.Id.Count > 0)
+        if (request.Id != null && request.Id.Count > 0)
             filter = filter.And(c => request.Id.Contains(c.Id));
         if (!string.IsNullOrWhiteSpace(request.Name))
             filter = filter.And(s => s.NameEn.Contains(request.Name) || s.NameAr.Contains(request.Name));
