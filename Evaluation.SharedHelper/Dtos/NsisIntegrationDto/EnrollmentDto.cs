fabc4bdb5f7d0c6201d8cf2528df0cf193e188d4
t-m.abutarbush <t-m.abutarbush@edu.gov.qa>
2026-03-24 08:16:58 +0300

    Edit Classes

--- Evaluation.SharedHelper/Dtos/NsisIntegrationDto/EnrollmentDto.cs
+++ Evaluation.SharedHelper/Dtos/NsisIntegrationDto/EnrollmentDto.cs
@@ -1,5 +1,10 @@
 ﻿namespace Evaluation.SharedHelper.Dtos.NsisIntegrationDto;
 
-public class EnrollmentDto
+public class EnrollmentDto : NSISBaseDto
 {
+    public Guid SchoolId { get; set; }
+    public Guid EnrollmentId { get; set; }
+    public Guid ClassId { get; set; }
+    public Guid Userid { get; set; }
+    public string Usertype { get; set; }
 }
--- Evaluation.SharedHelper/Dtos/NsisIntegrationDto/StaffDto.cs
+++ Evaluation.SharedHelper/Dtos/NsisIntegrationDto/StaffDto.cs
@@ -6,6 +6,16 @@ using System.Threading.Tasks;
 
 namespace Evaluation.SharedHelper.Dtos.NsisIntegrationDto;
 
-public class StaffDto
+public class StaffDto : NSISBaseDto
 {
+    public Guid SchoolId { get; set; }
+    public string QID { get; set; }
+    public string Name { get; set; }
+    public string EnglishName { get; set; }
+    public Guid JobFunctionID { get; set; }
+    public Guid TeachingAssignmentID { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Phones { get; set; }
+    public string Address { get; set; }
 }
