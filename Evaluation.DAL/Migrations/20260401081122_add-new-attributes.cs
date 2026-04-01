e19cbb92d6510619c57c5f764e98fde689ca0eae
Feras Mutasem Dev <t-f.mutasem-Dev@edu.gov.qa>
2026-04-01 11:58:54 +0300

    handle database Migration Database

---
+++ Evaluation.DAL/Migrations/20260401081122_add-new-attributes.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Evaluation.DAL.Migrations
+{
+    public partial class addnewattributes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ServiceStatusTypeId",
+                table: "ServiceStatus",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            // =========================
+            // ServiceRequests
+            // =========================
+            migrationBuilder.DropColumn(
+                name: "Sequence",
+                table: "ServiceRequests");
+
+            migrationBuilder.AddColumn<long>(
+                name: "Sequence",
+                table: "ServiceRequests",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            // =========================
+            // EvaluationRequest
+            // =========================
+            migrationBuilder.DropColumn(
+                name: "Sequence",
+                table: "EvaluationRequest");
+
+            migrationBuilder.AddColumn<long>(
+                name: "Sequence",
+                table: "EvaluationRequest",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L)
+                .Annotation("SqlServer:Identity", "1, 1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // الرجوع ServiceStatusTypeId
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ServiceStatusTypeId",
+                table: "ServiceStatus",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            // =========================
+            // ServiceRequests (إزالة Identity)
+            // =========================
+            migrationBuilder.DropColumn(
+                name: "Sequence",
+                table: "ServiceRequests");
+
+            migrationBuilder.AddColumn<long>(
+                name: "Sequence",
+                table: "ServiceRequests",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            // =========================
+            // EvaluationRequest
+            // =========================
+            migrationBuilder.DropColumn(
+                name: "Sequence",
+                table: "EvaluationRequest");
+
+            migrationBuilder.AddColumn<long>(
+                name: "Sequence",
+                table: "EvaluationRequest",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+    }
+}
