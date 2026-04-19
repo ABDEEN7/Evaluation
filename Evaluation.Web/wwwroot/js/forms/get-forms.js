716f5399df4bf9453e27e1f91ed396bc6d643eac
t-m.abutarbush <t-m.abutarbush@edu.gov.qa>
2026-04-19 10:35:01 +0300

    fix rename form

--- Evaluation.Web/wwwroot/js/forms/get-forms.js
+++ Evaluation.Web/wwwroot/js/forms/get-forms.js
@@ -92,7 +92,7 @@ const generateFormAccordionItem = (rowsHtml, hasAnyNote, hasAnyChildren, isRenam
                     ${rowsHtml}
                 </tbody>
             </table>
-            ${allowAdd ?`<div><button type="button" onclick="addNewRow()" class="btn btn-sm"><i class="la la-plus"></i> Add New</button></div>`:''}
+            ${allowAdd ?`<div><button type="button" onclick="addNewRow(this)" class="btn btn-sm add-btn"><i class="la la-plus"></i> Add New</button></div>`:''}
         </div>
     </div>
 </div>
@@ -261,7 +261,7 @@ const generateTableBodyHtml = async (
 
 };
 
-function addNewRow()
+function addNewRow(button)
 {
     if (renameItems.length > 0) {
 
@@ -289,9 +289,8 @@ function addNewRow()
         );
 
         renameItems.shift()
-        if (renameItems.length > 0)
-        {
-            //hide add button
+        if (renameItems.length == 0) {
+            button.style.display = "none";
         }
     }
     else
@@ -536,5 +535,16 @@ function deleteRow(button)
 
     if (row) {
         row.remove();
+        const rows = document.querySelectorAll("#tbodyRows tr");
+        var lastIndex = 0;
+        rows.forEach((tr, index) => {
+            tr.children[0].textContent = index + 1;
+            lastIndex = index + 1;
+        });
+        lastOrder = lastIndex;
+    }
+
+    if (renameItems.length > 0) {
+        $(".add-btn").show();
     }
 }
