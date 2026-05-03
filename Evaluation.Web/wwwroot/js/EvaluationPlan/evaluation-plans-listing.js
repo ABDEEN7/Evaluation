4ad9059f25b1661bb9e57ce45b083a761fc363a5
Feras Mutasem Dev <t-f.mutasem-Dev@edu.gov.qa>
2026-05-03 11:51:57 +0300

    handle Update Plan By Permission

--- Evaluation.Web/wwwroot/js/EvaluationPlan/evaluation-plans-listing.js
+++ Evaluation.Web/wwwroot/js/EvaluationPlan/evaluation-plans-listing.js
@@ -220,6 +220,8 @@
 
             const el = document.getElementById("CreateRequestModal");
             const modal = bootstrap.Modal.getOrCreateInstance(el);
+            formUtility.addQueryParameter('serviceId', serviceId);
+            formUtility.addQueryParameter('planId', planId);
             modal.show();
 
             const serviceName = createPlanRequestService.name;
