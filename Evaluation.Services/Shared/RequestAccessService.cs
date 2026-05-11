96f8d6f3bcb9f65943ca3bd2f4521552f7122921
Aya Abdalla <t-a.abdalla@edu.gov.qa>
2026-05-11 13:18:06 +0300

    ApplyPlanAccess

--- Evaluation.Services/Shared/RequestAccessService.cs
+++ Evaluation.Services/Shared/RequestAccessService.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Evaluation.DAL.Helper;
+using Evaluation.DAL.Models.Planing;
 using Evaluation.DAL.Models.Planing.EvaluationRequestEntity;
 using Evaluation.DAL.Models.ServiceRequestEntities;
 using Evaluation.DAL.Models.UserEntiy;
@@ -119,6 +120,44 @@ namespace Evaluation.Services.Shared
 				x.Status.StatusPreventPartyTypes != null &&
 				!x.Status.StatusPreventPartyTypes.Any(p =>
 					userPartyTypeIds.Contains(p.PartyTypeId)));
+
+			return query;
+		}
+
+		public async Task<IQueryable<Plan>> ApplyPlanAccess(IQueryable<Plan> query)
+		{
+			var userId = userInfo.UserId
+				?? throw new BusinessException(ExceptionMessage.UserNotFound);
+
+			var user = await srvUser.GetByIDActiveNonDeleted(userId);
+
+			if (user == null)
+				throw new BusinessException(ExceptionMessage.UserNotFound);
+
+			var canViewAllPlans = user.UserPartTypes?
+				.Any(x => x.PartyType != null &&
+						  x.PartyType.CanViewAllPlan == true) == true;
+
+			var canViewEvlRequestPlan = user.UserPartTypes?
+				.Any(x => x.PartyType != null &&
+						  x.PartyType.CanViewEvlRequetPlan == true) == true;
+
+			if (canViewAllPlans)
+				return query;
+
+			if (canViewEvlRequestPlan)
+			{
+				query = query.Where(plan =>
+					plan.EvaluationRequests != null &&
+					plan.EvaluationRequests.Any(ev =>
+						ev.EvaluationRequestAssignments != null &&
+						ev.EvaluationRequestAssignments.Any(a =>
+							a.MinistryUserId == userId)));
+			}
+			else
+			{
+				query = query.Where(x => false);
+			}
 
 			return query;
 		}
