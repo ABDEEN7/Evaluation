d5e6d6ba86bc0e51aa84f02d170102e3ac99e8e2
Feras Mutasem Dev <t-f.mutasem-Dev@edu.gov.qa>
2026-05-14 14:12:33 +0300

    reflect new changes in department-holiday and added Comment Permissions to Action Controller

--- Evaluation.API/Controllers/TemplateFormController.cs
+++ Evaluation.API/Controllers/TemplateFormController.cs
@@ -146,6 +146,7 @@ public class TemplateFormController : ControllerBase
 
     }
     [HttpGet]
+    //[CheckRolePermisionFilter(true, PermisionNames: new[] { ConstantKeys.WebPermissions.VIEW_WEB_FORMITEMS })]
     public async Task<IActionResult> GetEvalFormItemLists(Guid formId)
     {
         Dictionary<string, object> response = new Dictionary<string, object>();
@@ -159,24 +160,28 @@ public class TemplateFormController : ControllerBase
     }
    
     [HttpGet]
+    //[CheckRolePermisionFilter(true, PermisionNames: new[] { ConstantKeys.WebPermissions.VIEW_WEB_FORMITEMCONFIG })]
     public async Task<IActionResult> GetAllFormItemConfig(Guid? evalFormId)
     {
         var data = await _masterBl.GetApiService<EvaluationFormBL>().GetAllFormItemConfig(evalFormId);
         return Ok(data);
     }
     [HttpPost]
+    //[CheckRolePermisionFilter(true, PermisionNames: new[] {ConstantKeys.WebPermissions.ADD_WEB_FORMITEMCONFIG})]
     public async Task<IActionResult> SaveFormItemConfig()
     {
         var request = Request.Form["request"][0]?.StringToObject<List<CreateFormItemConfigDto>>();
         return Ok(await _masterBl.GetApiService<EvaluationFormBL>().SaveFormItemConfig(request!));
     }
     [HttpPost]
+    //[CheckRolePermisionFilter(true, PermisionNames: new[] { ConstantKeys.WebPermissions.EDIT_WEB_FORMITEMCONFIG })]
     public async Task<IActionResult> UpdateFormItemConfig()
     {
         var request = Request.Form["request"][0]?.StringToObject<CreateFormItemConfigDto>();
         return Ok(await _masterBl.GetApiService<EvaluationFormBL>().UpdateFormItemConfig(request!));
     }
     [HttpPost]
+    //[CheckRolePermisionFilter(true, PermisionNames: new[] { ConstantKeys.WebPermissions.DELETE_WEB_FORMITEMCONFIG })]
     public async Task<IActionResult> DeleteFormItemConfig(Guid formId)
     {
         return Ok(await _masterBl.GetApiService<EvaluationFormBL>().DeleteFormItemConfig(formId));
