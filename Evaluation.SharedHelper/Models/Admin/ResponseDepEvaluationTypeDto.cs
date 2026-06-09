e0a17554a0d2c22e2f5b199d1d2b57dbf694cd20
Feras Mutasem Dev <t-f.mutasem-Dev@edu.gov.qa>
2026-06-09 08:41:43 +0300

    Handle Last Issues in Admin Portal

---
+++ Evaluation.SharedHelper/Models/Admin/ResponseDepEvaluationTypeDto.cs
@@ -0,0 +1,13 @@
+﻿using Evaluation.DAL.Models.DepartementEntites;
+
+namespace Evaluation.SharedHelper.Models.Admin;
+
+public class ResponseDepEvaluationTypeDto : EntityBaseDTO
+{
+    public string Department { get; set; }
+    public string EvaluationType { get; set; }
+    public string BackendName { get; set; } = null!;
+    public string NameAr { get; set; } = null!;
+    public string NameEn { get; set; } = null!;
+    public int OrderNo { get; set; }
+}
