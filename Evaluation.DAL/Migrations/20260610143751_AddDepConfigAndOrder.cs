4e8b5bbf5330302bd87bac6b76846441b79ab7a6
Mohamed Fatouh (EBLA) <t-m.fatouh-dev@edu.gov.qa>
2026-06-10 17:39:21 +0300

    DB Migration  20260610143751_AddDepConfigAndOrder

---
+++ Evaluation.DAL/Migrations/20260610143751_AddDepConfigAndOrder.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Evaluation.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDepConfigAndOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OrderNo",
+                table: "DepWebGroup",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DepConfig",
+                table: "Departments",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderNo",
+                table: "DepWebGroup");
+
+            migrationBuilder.DropColumn(
+                name: "DepConfig",
+                table: "Departments");
+        }
+    }
+}
