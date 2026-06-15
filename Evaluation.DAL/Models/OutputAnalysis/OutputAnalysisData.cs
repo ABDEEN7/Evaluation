d622f01acb9f6ec88e5090d45f4301b9973453e2
Mohamed Fatouh (EBLA) <t-m.fatouh-dev@edu.gov.qa>
2026-06-16 01:25:21 +0300

    Db Migration 20260615222321_AddOutputAnalysisModule

---
+++ Evaluation.DAL/Models/OutputAnalysis/OutputAnalysisData.cs
@@ -0,0 +1,34 @@
+﻿using Evaluation.DAL.Models.Audit;
+using Evaluation.DAL.Models.BaseModule;
+using Evaluation.DAL.Models.FormsModules;
+using Evaluation.DAL.Models.Planing.EvaluationRequestEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluation.DAL.Models.OutputAnalysis
+{
+    public class OutputAnalysisData : EntityBase, IAuditLogEntity
+    {
+        public Guid OutputAnalysisFinalResultId { get; set; }
+        public OutputAnalysisFinalResult? OutputAnalysisFinalResult { get; set; }
+        public Guid AnalysisTypeId { get; set; }
+        public AnalysisType? AnalysisType { get; set; }
+        public Guid EvaluationRequestId { get; set; }
+        public EvaluationRequest? EvaluationRequest { get; set; }
+        public int Grade { get; set; }
+        public int LastYear { get; set; }
+        public int PreviousYear { get; set; }
+        public decimal LastYearValue { get; set; }
+        public decimal PreviousYearValue { get; set; }
+        public decimal Difference { get; set; }
+        public string? SubjectCode { get; set; }
+        public string? Track { get; set; }
+        public Guid? FormEvalMatrixValueId { get; set; }
+        public FormEvalMatrixValue? FormEvalMatrixValue { get; set; }
+        public decimal ActualValue { get; set; }
+        public string? Note { get; set; }
+    }
+}
