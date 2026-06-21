2eb57c8cbe3586a313fb9bb3afb10470bc10208a
Mohamed Fatouh (EBLA) <t-m.fatouh-dev@edu.gov.qa>
2026-06-21 12:28:24 +0300

    Db MIgration 20260621092630_AddItemNumberToFormItem

---
+++ Evaluation.DAL/Migrations/20260621092630_AddItemNumberToFormItem.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Evaluation.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddItemNumberToFormItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ScopeNumber",
+                table: "Scopes",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ItemNumber",
+                table: "FormItem",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ScopeNumber",
+                table: "Scopes");
+
+            migrationBuilder.DropColumn(
+                name: "ItemNumber",
+                table: "FormItem");
+        }
+    }
+}
