d9abc440423b39237cfcedb980397f7d4f7d8250
Feras Mutasem Dev <t-f.mutasem-Dev@edu.gov.qa>
2026-06-24 08:36:12 +0300

    disply  range

--- Evaluation.Services/BusinessLayer/Admin/SrvFormEvalMarixValueBL.cs
+++ Evaluation.Services/BusinessLayer/Admin/SrvFormEvalMarixValueBL.cs
@@ -59,6 +59,11 @@ namespace Evaluation.Services.Models.Admin
                 obj.DescAr = message.DescAr;
                 obj.DescEn = message.DescEn;
                 obj.ColorCode = message.ColorCode;
+                obj.ReportTextAr = message.ReportTextAr;
+                obj.ReportTextEn = message.ReportTextEn;
+                obj.ReportDescAr = message.ReportDescAr;
+                obj.ReportDescEn = message.ReportDescEn;
+                obj.DisplayRange = message.DisplayRange;
                 obj.IsActive = message.IsActive;
 
                 uow.GetRepository<FormEvalMatrixValue>().Insert(obj);
@@ -97,6 +102,10 @@ namespace Evaluation.Services.Models.Admin
                 obj.NextEvalDays = message.NextEvalDays;
                 obj.ActualMatrixValue = message.ActualMatrixValue;
                 obj.ColorCode = message.ColorCode;
+                obj.ReportTextAr = message.ReportTextAr;
+                obj.ReportTextEn = message.ReportTextEn;
+                obj.ReportDescAr = message.ReportDescAr;
+                obj.ReportDescEn = message.ReportDescEn;
                 obj.IsActive = message.IsActive;
 
                 uow.GetRepository<FormEvalMatrixValue>().Update(obj);
