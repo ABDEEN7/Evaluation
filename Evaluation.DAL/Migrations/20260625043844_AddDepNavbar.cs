8360befdad45f578d087643db683974d85e420e8
Mohamed Fatouh (EBLA) <t-m.fatouh-dev@edu.gov.qa>
2026-06-25 07:40:35 +0300

    Db Migration 20260625043844_AddDepNavbar

---
+++ Evaluation.DAL/Migrations/20260625043844_AddDepNavbar.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Evaluation.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDepNavbar : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "DepartmentId",
+                table: "Navbar",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Navbar_DepartmentId",
+                table: "Navbar",
+                column: "DepartmentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Navbar_Departments_DepartmentId",
+                table: "Navbar",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Navbar_Departments_DepartmentId",
+                table: "Navbar");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Navbar_DepartmentId",
+                table: "Navbar");
+
+            migrationBuilder.DropColumn(
+                name: "DepartmentId",
+                table: "Navbar");
+        }
+    }
+}
